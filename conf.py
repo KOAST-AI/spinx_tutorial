653c93e43dd0444b3bba3b3f2680ffb7754adc32
unknown <szwlemona@naver.com>
2020-12-04 10:31:06 +0900

    Intro6, modify index7 page

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'test3'
+project = 'sphinx'
 copyright = '2020, szw'
 author = 'szw'
 
